5ace249d097d8614a04e024593f42f5bf42c8e5a
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-21 20:22:18 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// js/main.js
+
+// Import colorChange functionality
+import './color/colorChange.js';
+
+// You can put other initialization code here in the future
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Main script loaded.');
+});
